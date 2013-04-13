25164a65abae6f4534526af662891dc0814ca8eb
ghostologist <ghostologist@enki.(none)>
2013-04-13 11:43:34 -0400

    Added the basic role assignment procedure

--- src/CompetitorAI.java
+++ src/CompetitorAI.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 import bonzai.api.AI;
 import bonzai.api.Farmhand;
@@ -8,16 +9,86 @@ import bonzai.api.GameState;
 
 
 public class CompetitorAI implements AI {
-
+	
+	//Codes for role assignment
+	private final int R_DUCK_FETCH = 0;
+	private final int R_RECON = 1;
+	private final int R_GRIEF = 2;
+	private final int R_PATHS = 3;
+	
+	private HashMap<Integer, Integer> roles = new HashMap<Integer, Integer>();
+	
 	@Override
 	public Collection<FarmhandAction> turn(GameState state) {
 		ArrayList<FarmhandAction> actions = new ArrayList<FarmhandAction>();
 		
-		for (Farmhand farmhand : state.getMyFarmhands().getNotStumbled()) {
-			actions.add(farmhand.shout("Hello"));
+		//Number of workers to assign to each role
+		int totalWorkers = state.getMyFarmhands().size();
+		int duckFetch = totalWorkers/2;
+		int recon = 1;
+		int grief = 1;
+		int pathbuilding = totalWorkers - duckFetch - recon - grief;
+		
+		//role assignment
+		for (int index = 0; index < totalWorkers; index++) {
+			if (duckFetch > 0) {
+				roles.put(index, R_DUCK_FETCH);
+				duckFetch--;
+			}
+			else if (recon > 0) {
+				roles.put(index,  R_RECON);
+				recon--;
+			}
+			else if (grief > 0) {
+				roles.put(index, R_GRIEF);
+				grief--;
+			}
+			else if (pathbuilding > 0) {
+				roles.put(index, R_PATHS);
+				pathbuilding--;
+			}
+		}
+		
+		/* Print out what each farmhand is going to do for this turn
+		for (Integer i : roles.keySet()) {
+			System.out.println("Farmhand " + i + " is doing " + roles.get(i));
+		}
+		*/
+		
+		int index = 0;
+		for (Farmhand farmhand : state.getMyFarmhands()) {
+			if (roles.get(index) == R_DUCK_FETCH)
+				actions.add(duckFetch(state, farmhand));
+			else if (roles.get(index) == R_RECON)
+				actions.add(recon(state, farmhand));
+			else if (roles.get(index) == R_GRIEF)
+				actions.add(grief(state, farmhand));
+			else if (roles.get(index) == R_PATHS)
+				actions.add(buildPaths(state, farmhand));
+			else
+				actions.add(noJob(state, farmhand));
 		}
 		
 		return actions;
 	}
 
+	private FarmhandAction noJob(GameState state, Farmhand farmhand) {
+		return farmhand.shout("I'm lazy and have no job");
+	}
+
+	private FarmhandAction buildPaths(GameState state, Farmhand farmhand) {
+		return farmhand.shout("I'm building paths");
+	}
+
+	private FarmhandAction grief(GameState state, Farmhand farmhand) {
+		return farmhand.shout("I'm griefing");
+	}
+
+	private FarmhandAction recon(GameState state, Farmhand farmhand) {
+		return farmhand.shout("I'm reconing");
+	}
+
+	private FarmhandAction duckFetch(GameState state, Farmhand farmhand) {
+		return farmhand.shout("I'm fetching ducks");
+	}
 }
